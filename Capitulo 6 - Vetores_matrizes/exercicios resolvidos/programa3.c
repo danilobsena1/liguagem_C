29036957b95c4a2812854fdc4f735d16d2361649
Danilo Barros <danilo_barross@outlook.com>
2022-06-27 23:56:25 -0300

    update exercicio

--- Capitulo 6 - Vetores_matrizes/exercicios resolvidos/programa3.c
+++ Capitulo 6 - Vetores_matrizes/exercicios resolvidos/programa3.c
@@ -9,10 +9,25 @@ int main(){
     int vetor [7][7];
     int numero;
 
-    printf("Quantos números quer apostar( 0-Sair\n");
-    printf("Número: ");
-    scanf("%d",&numero);
-    
+    srand(time(NULL)); 
+
+    for(int i = 0; i<7; i++){
+        for(int j= 0; j<7;j++){
+            vetor[i][j] = rand()%50; // Gera valores aleatorios.
+        }
+    }
+
+   for(int i = 0; i<7; i++){
+        for(int j= 0; j<7;j++){
+            printf("%d ",vetor[i][j]);
+        }
+        printf("\n");
+    }
+
+    // printf("Quantos números quer apostar( 0-Sair\n");
+    // printf("Número: ");
+    // scanf("%d",&numero);
+
     
     return 0;
 }
